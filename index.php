5fe7f47e410ca985bad761ad2c4377c24e92aed5
solecardazzo <soledadcardazzo@gmail.com>
2021-11-04 12:49:58 -0300

    script hecho

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$nombre = 'Soledad';
+$apellido = 'Cardazzo';
+$frase = "Mi nombre es $nombre $apellido\n";
+echo nl2br($frase);
+$edad = 39;
+$frase = "Tengo $edad años\n";
+echo nl2br($frase);
+$hobbie = 'programar';
+$frase = "Mi hobbie es $hobbie\n";
+echo nl2br($frase);
+$editor = 'Atom';
+$frase = "Mi editor de codigo preferido es $editor\n";
+echo nl2br($frase);
+$so = 'Windows';
+$frase = "Actualmente mi sistema operativo es $so\n";
+echo nl2br($frase);
+?>
